f1d5d548facaf579b431cdafc530a742a44afacb
Ke An <limkean10@gmail.com>
2023-03-11 10:49:40 +0800

    Rebuild to test deployment

--- dist/assets/add-215fed47.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,a as t}from"./index-2c36887b.js";const s={xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"2",stroke:"#fff",class:"w-6 h-6"},n=t("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M12 4.5v15m7.5-7.5h-15"},null,-1),r=[n];function c(a,d){return e(),o("svg",s,r)}const i={render:c};export{i as default,c as render};
